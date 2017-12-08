d0889cea6e3ca83af2438b30e8d6aa6988d8de1f
liudongguang <liudongguang@qq.com>
2017-12-08 09:23:25 +0800

    20171208 手术

--- src/main/java/com/peony/remote/sdey/api/po/SSXX_anpai.java
+++ src/main/java/com/peony/remote/sdey/api/po/SSXX_anpai.java
@@ -6,6 +6,51 @@ public class SSXX_anpai {
     private Date ssrq;//安排手术日期
     private String ssfj;//手术分级
     private String brks;//病人科室
+    private String ssmc;//手术名称
+    private String brxm;//病人姓名
+    private String ssys;//手术医生
+    private Integer brnl;//病人年龄
+    private String brxb;//病人性别
+
+    public String getSsmc() {
+        return ssmc;
+    }
+
+    public void setSsmc(String ssmc) {
+        this.ssmc = ssmc;
+    }
+
+    public String getBrxm() {
+        return brxm;
+    }
+
+    public void setBrxm(String brxm) {
+        this.brxm = brxm;
+    }
+
+    public String getSsys() {
+        return ssys;
+    }
+
+    public void setSsys(String ssys) {
+        this.ssys = ssys;
+    }
+
+    public Integer getBrnl() {
+        return brnl;
+    }
+
+    public void setBrnl(Integer brnl) {
+        this.brnl = brnl;
+    }
+
+    public String getBrxb() {
+        return brxb;
+    }
+
+    public void setBrxb(String brxb) {
+        this.brxb = brxb;
+    }
 
     public Date getSsrq() {
         return ssrq;
